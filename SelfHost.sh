0ea15e38ac236672784152eb23b1afb97b2f3934
delucac <delucac@sunypoly.edu>
2022-04-06 22:02:24 -0400

    Added module for testing purposes

--- SelfHost.sh
+++ SelfHost.sh
@@ -23,18 +23,57 @@ echo "0. About\n"
 echo "E. Exit"
 
 read -p "Please make a selection: " choice
-
+echo "\n"
 case $choice in
 	1)
-		mkdir data 
+		if [ -d ./data/ ]
+		then
+			echo "Data files already exist\n"
+		fi
+
+		if [ ! -d ./data/ ]
+		then
+			echo "Creating data files\n"
+			mkdir data
+		fi
+		if [ -d ./modules/ ]
+		then
+			echo "modules folder exists\n"
+		fi
+		if [ ! -d ./modules/ ]
+		then
+			echo "Creating modules folder\n"
+			mkdir modules
+		fi
+	;;
+	2)
+		if [ -e "./modules/staticWeb.sh" ]
+		then
+			./modules/staticWeb.sh
+			echo "Returning to main script\n"
+		fi
+
+		if [ ! -e "./modules/staticWeb.sh" ]
+		then
+			echo "Module not present"
+			read -p "Would you like to download the file? (y/n): " download
+			if [ $download = "y" || $download = "Y" ]
+			then
+				echo "download"
+			fi
+		fi
+
 	;;
 
+	#Two cases for user error
 	e)
 		run=0
 	;;
 	E)
 		run=0
 	;;
+
+	#Error checking
 	*)
 		clear
 		echo "Error: invalid value"
---
+++ modules/staticWeb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# File name: staticWeb.sh
+# Author: Christopher B. DeLuca
+# Date started: April 6th, 2022
+# Description:
+# Script for installing a static webserver
+#
+#
+#
+echo "staticWeb.sh"
+
+exit 0
